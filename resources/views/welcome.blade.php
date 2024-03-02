7b48dc6e25533661b321ea76a4d4351779bf50c1
Eojin Kim <eojin1211@hanmail.net>
2024-03-02 18:38:13 +0900

    c3c240d revised content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,43 +1,26 @@
 <x-app-layout>
-  <div class="w-full" x-data="{memo: null}">
-    <div class="w-full">
-      <textarea name="memo" x-model="memo" class="w-full rounded border border-gray-200 shadow-inner h-32 md:h-36"
-        placeholder="This is a memo app. Just add your idea here and hit that big button below." required></textarea>
-    </div>
-    <button class="block w-full p-2 bg-binder text-white rounded disabled:bg-binder-300 font-bold"
-      :disabled="memo === null || memo.trim().length === 0"
-      x-on:click="alert('technically you need to sign in, but this is how it ALWAYS works.')" type="button"
-      disabled>MEMMO</button>
+  <div>
+    <h1 class="mb-3">
+      <span class="text-2xl">This is a memo app,</span><br>
+      <span class="text-xl">but with no "add new" button.</span>
+    </h1>
+    <p class="ms-3">Instead you're given with a form like this. Now you can't forget how to compose new notes here.</p>
   </div>
-  <div class="pt-6"></div>
-  <div class="grid grid-cols-1 divide-y">
-    <div class="border-binder-200 p-3" x-data="{open: false}">
-      <div class="flex justify-between">
-        <span class="me-3">note app without "add" button</span>
-        <button class="text-nowrap cursor-pointer text-binder-500 hover:text-binder-800" x-on:click="open = !open;">
-          3 minutes ago
-        </button>
-      </div>
-      <div class="mt-3" x-show="open" x-cloak>
-        <div class="prose">
-          <p>To my surprise, it was really difficult for me to find a note app that allows me start writing on the main view. Instead, almost every note app has "add" button. I don't get it. Why? Is that how the legal pads in the real world work? You press a button before scribbling on the paper?</p>
-          <p>So I decided to build what I want iOS Notes app to be like back in 2017. This is a rebooted version of it -- TALL stack.</p>
-        </div>
-      </div>
+  <div class="w-full py-6">
+    <div class="w-full">
+      <textarea name="memo" x-model="memo" class="w-full rounded border border-gray-200 bg-gray-300 shadow-inner h-32 md:h-36"
+        placeholder="Here you add your notes and hit 'MEMMO' button below, and your note will be saved." disabled></textarea>
     </div>
-    <div class="border-binder-200 p-3" x-data="{open: false}">
-      <div class="flex justify-between">
-        <span class="me-3">never to be "knowledgebase solution"</span>
-        <button class="text-nowrap cursor-pointer text-binder-500 hover:text-binder-800" x-on:click="open = !open;">
-          7 minutes ago
-        </button>
-      </div>
-      <div class="mt-3" x-show="open" x-cloak>
-        <div class="prose">
-          <p>A NOTEPAD IS A NOTEPAD. Physical legal pads exist to collect your scribbles on sheets of paper and no further than that.</p>
-          <p>So will be Memmo -- it will not gonna arrange your schedules, prioritize your todos, version control your pictures or source codes, aggregate what artifacts are most contributing to your per-seat-subscription-based team. it will just sit calm as your old school notepad -- lightweight, least ambitious, doing what it does as expected.</p>
-        </div>
-      </div>
+    <button class="block w-full p-2 bg-binder text-white rounded disabled:bg-binder-300 font-bold" type="button" disabled>MEMMO</button>
+  </div>
+  <div>
+    <h2 class="mb-3">
+      <span class="text-xl">Straight and hassle-free,</span><br>
+      <span class="text-lg">just as any kind of notepad should be.</span>
+    </h2>
+    <div class="ms-3">
+      <p class="mb-3">MEMMO is not going to be a "knowledgebase management app" -- it's a virtual legal pad after all.</p>
+      <p class="mb-3">You come up with something, you open MEMMO, you dump it here, you forget and move on. Life can be that simple.</p>
     </div>
   </div>
 </x-app-layout>
