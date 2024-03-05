802a339d53056eb4104e3ef4326decf1bfa8cdd1
Eojin Kim <eojin1211@hanmail.net>
2024-03-05 19:44:51 +0900

    0a67bc1286 retry

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome');
+Route::get('/', fn () => view(Auth::check() ? 'memmo' : 'welcome'));
 
 Route::view('memmo', 'memmo')
     ->middleware(['auth', 'verified'])
