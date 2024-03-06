cee25c782ba69025df22e8e51cbdff1d04f2b555
Eojin Kim <eojin1211@hanmail.net>
2024-03-06 11:08:25 +0900

    minor memmo form improvements
    - auto focus to textarea
    - focus color improved

--- resources/views/livewire/memmo/form.blade.php
+++ resources/views/livewire/memmo/form.blade.php
@@ -2,12 +2,13 @@
     @csrf
     <div class="w-full">
         <textarea name="memo"
-            class="w-full rounded border border-gray-200 shadow-inner h-32 md:h-36"
+            class="w-full rounded border border-gray-200 shadow-inner h-32 md:h-36 focus:ring-paper-600 focus:border-paper-600"
             wire:model="memo" x-model="memo"
+            x-init="$el.focus()"
             placeholder="{{ $placeholder }}" required></textarea>
     </div>
     <button
-        class="block w-full p-2 bg-binder text-white rounded disabled:bg-binder-300 font-bold"
+        class="block w-full p-2 bg-binder text-white rounded disabled:bg-binder-300 font-bold focus-visible:outline-binder-900"
         :disabled="memo === null || memo.trim().length === 0"
         type="submit" disabled>MEMMO</button>
 </form>
