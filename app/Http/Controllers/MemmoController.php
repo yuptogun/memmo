293d7b60947cffcc37a06f294f923edba9d4dea3
Eojin Kim <eojin1211@hanmail.net>
2024-03-09 20:10:34 +0900

    added view using Controller

--- app/Http/Controllers/MemmoController.php
+++ app/Http/Controllers/MemmoController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
+
+use App\Models\Config;
 
 class MemmoController extends Controller
 {
-    //
+    public function index()
+    {
+        return view(Auth::check() ? 'memmo' : 'welcome');
+    }
+
+    public function showShared(string $shareCode)
+    {
+        $memmo = Cache::remember("memmo:shared:$shareCode", 86400, function () use ($shareCode) {
+            $memmo = Config::where(fn ($where) =>
+                $where->where(fn ($w1) => $w1->keyValue('share_code_alias', $shareCode))
+                    ->orWhere(fn ($w2) => $w2->keyValue('share_code', $shareCode))
+            )->firstOrFail()->configurable;
+            return $memmo && $memmo->is_shared ? $memmo : null;
+        });
+
+        return $memmo ? view('show-public', ['memmo' => $memmo]) : abort(404);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\MemmoController;
 
-Route::get('/', fn () => view(Auth::check() ? 'memmo' : 'welcome'));
-
-Route::view('memmo', 'memmo')
-    ->middleware(['auth', 'verified'])
-    ->name('memmo');
+Route::controller(MemmoController::class)->group(function () {
+    Route::get('/',              'index')->name('index');
+    Route::get('/m/{shareCode}', 'showShared')->name('show-shared');
+});
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
