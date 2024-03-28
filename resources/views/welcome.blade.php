61ee59931536f7e799ce371418d8a366f56d62f5
Eojin Kim <eojin1211@hanmail.net>
2024-03-28 20:37:07 +0900

    fixed introduction

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,26 +1,23 @@
 <x-app-layout>
   @section('title', config('app.name') . ': one field, one button.')
-  <div>
+  {{-- <div>
     <h1>
       <span class="text-2xl">Meet <x-application-logo></x-application-logo>,</span><br>
       <span class="text-xl">a note app with one field and one button.</span>
     </h1>
-  </div>
-  <div class="w-full py-6">
+  </div> --}}
+  <div class="w-full">
     <div class="w-full">
-      <textarea class="w-full rounded border border-gray-200 h-32 bg-gray-200 shadow-inner"
-        placeholder="Add your ideas here. Hit the button. That's it." disabled></textarea>
+      <textarea class="w-full rounded border border-gray-200 h-32" name="memo"
+        placeholder="Add your ideas here. Hit the button. That's it."></textarea>
     </div>
-    <button class="block w-full p-2 bg-binder text-white rounded disabled:bg-binder-300 font-bold" type="button" disabled>MEMMO</button>
+    <button class="block w-full p-2 bg-binder text-white rounded font-bold focus-visible:outline-binder-900" type="button" onclick="alert('sorry, demo only! please login to use.');">MEMMO</button>
   </div>
-  <div>
-    <h2 class="mb-3">
-      <span class="text-xl">It's your idea outlet.</span><br>
-      <span class="text-lg">It will save your day when you're in a greatest hurry.</span>
-    </h2>
-    <div class="mx-3">
-      <p class="mb-3">To my surprise, most of the note apps have buttons like "add", "new", "+". I never got used to find and click those buttons because who cares the buttons?? My ideas are already vaporizing! I don't need any more buttons, I need a toilet that I can rush into, write my shit down and flush!!</p>
-      <p class="mb-3">So I decided to incarnate what virtual legal pads should look like. The first version is made in 2019, this one 2024.</p>
+  <div class="py-6">
+    <h2 class="my-6 text-center text-lg">This I believe is how every notepad (app) works.</h2>
+    <div class="mx-3 prose">
+      <p>It must provide a blank that I can immediately start filling in -- it should never be hidden behind any buttons like "add", "new" or "+". one field, one button. maybe then a list. that's not too much to ask, ain't it?</p>
+      <p>The first version was made in 2019. Now you're seeing the 2024 reboot. please log in and enjoy your virtual notepad.</p>
     </div>
   </div>
 </x-app-layout>
